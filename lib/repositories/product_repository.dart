8009ea045bb0a8179849f874f4828469ad08557f
hasti <hasti@localhost>
2026-08-02 18:42:56 +0330

    Deleted redundant try/catch blocks.

--- lib/repositories/product_repository.dart
+++ lib/repositories/product_repository.dart
@@ -37,38 +37,22 @@ class ProductRepository {
   }
 
   Future<void> addProduct(ProductModel product) async {
-    try {
       await localDataSource.addProduct(product);
-    } catch (e) {
-      throw Exception('Error Adding Product: $e');
-    }
   }
 
   Future<void> updateProduct(ProductModel product) async {
-    try {
       await localDataSource.updateProduct(product);
-    } catch (e) {
-      throw Exception('Error Updating Product: $e');
-    }
   }
 
   Future<void> deleteProduct(ProductModel product) async {
-    try {
       await localDataSource.deleteProduct(product);
-    } catch (e) {
-      throw Exception('Error Deleting Product: $e');
-    }
   }
 
   Future<void> setDeleted(ProductModel product) async {
-    try {
       await localDataSource.updateProductSyncStatus(
         product,
         SyncStatus.pendingDelete,
       );
-    } catch (e) {
-      throw Exception('Error Deleting Product: $e');
-    }
   }
 
   Future<void> syncPendingProducts() async {
